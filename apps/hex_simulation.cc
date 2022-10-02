a6867bd825110f518518282ea510be6220e46ac1
Adeeb Abbas <aa4239@drexel.edu>
2022-10-02 12:07:46 -0400

    restructuring some code

--- apps/hex_simulation.cc
+++ apps/hex_simulation.cc
@@ -12,18 +12,22 @@ using drake::multibody::MultibodyPlant;
 using drake::multibody::MultibodyPlantConfig;
 
 using namespace drake; 
-int main() {
 
+int doMain() {
   systems::DiagramBuilder<double> builder;
+  MultibodyPlantConfig plant_config;
+  plant_config.time_step = 0.01;
+  plant_config.stiction_tolerance = 1.0E-3;
+  plant_config.discrete_contact_solver = "sap";
+    auto [plant, scene_graph] =
+    multibody::AddMultibodyPlant(plant_config, &builder);
 
-    MultibodyPlantConfig plant_config;
-    plant_config.time_step = 0.01;
-    plant_config.stiction_tolerance = 1.0E-3;
-    plant_config.discrete_contact_solver = "sap";
-      auto [plant, scene_graph] =
-      multibody::AddMultibodyPlant(plant_config, &builder);
+  const std::string full_name = "apps/models/urdf/phantomx.urdf";
+  multibody::Parser(&plant).AddModelFromFile(full_name);
+  return 0;
+}
 
-    const std::string full_name = "apps/models/urdf/phantomx.urdf";
-    multibody::Parser(&plant).AddModelFromFile(full_name);
+int main() {
+    doMain();
     return 0;
 }
