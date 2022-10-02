cffc12ceca5b95f71dfcc093c7de8a6dea7a658f
Adeeb Abbas <aa4239@drexel.edu>
2022-10-01 21:31:35 -0400

    model directory Bazel build file

--- apps/hex_simulation.cc
+++ apps/hex_simulation.cc
@@ -1,7 +1,19 @@
+// Sample placeholder for more code to come. 
+
 #include "drake/multibody/parsing/parser.h"
 #include "drake/multibody/plant/multibody_plant.h"
-
+#include <iostream>
+#include <fstream>
+#include <string>
 
 int main() {
+
+    // some dumb test to check we can import models here - 
+    std::ifstream myfile ("models/urdf/phantomx.urdf");
+    std::string mystring;
+    if ( myfile.is_open() ) { // always check whether the file is open
+    myfile >> mystring; // pipe file's content into stream
+    std::cout << mystring; // pipe stream's content to standard output
+    }
     return 0;
 }
